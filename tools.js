5a35f6385faef17fa5aa767004508b604fb7873a
Lekha <lekhasoni001@gmail.com>
2023-12-28 13:59:19 +0530

    sticky note

--- tools.js
+++ tools.js
@@ -5,6 +5,7 @@ let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolsCont = document.querySelector(".eraser-tool-cont"); 
 let pencil = document.querySelector(".pencil"); 
 let eraser = document.querySelector(".eraser"); 
+let sticky = document.querySelector(".sticky"); 
 let pencilFlag = false; 
 let eraserFlag = false; 
 
@@ -47,3 +48,72 @@ eraser.addEventListener("click", (e) => {
     else eraserToolsCont.style.display = "none";
 })
 
+sticky.addEventListener("click", (e) => {
+    let stickyCont = document.createElement("div"); 
+    stickyCont.setAttribute("class", "sticky-cont"); 
+    stickyCont.innerHTML = `
+    <div class="header-cont">
+            <div class="minimize"></div>
+            <div class="remove"></div>
+        </div>
+        <div class="notes-cont">
+            <textarea></textarea>
+        </div>
+        `; 
+    document.body.appendChild(stickyCont); //directly appending in body
+
+    let minimize = stickyCont.querySelector(".minimize"); 
+    let remove = stickyCont.querySelector(".remove"); 
+    noteAction(minimize, remove, stickyCont); 
+
+    stickyCont.onmousedown = function(event) {
+        dragAndDrop(stickyCont,event);
+      };
+      
+    stickyCont.ondragstart = function() {
+        return false;
+      };
+})
+
+function noteAction(minimize, remove, stickyCont){
+    remove.addEventListener("click", (e) => {
+        stickyCont.remove(); 
+    })
+    minimize.addEventListener("click", (e) => {
+        let noteCont = stickyCont.querySelector(".notes-cont"); 
+        let display = getComputedStyle(noteCont).getPropertyValue("display"); 
+        if (display === "none") noteCont.style.display = "block"; 
+        else noteCont.style.display = "none"; 
+    })
+
+}
+
+function dragAndDrop(element,event){
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+        let shiftY = event.clientY - element.getBoundingClientRect().top;
+      
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
+      
+        moveAt(event.pageX, event.pageY);
+      
+        // moves the element at (pageX, pageY) coordinates
+        // taking initial shifts into account
+        function moveAt(pageX, pageY) {
+          element.style.left = pageX - shiftX + 'px';
+          element.style.top = pageY - shiftY + 'px';
+        }
+      
+        function onMouseMove(event) {
+          moveAt(event.pageX, event.pageY);
+        }
+      
+        // move the element on mousemove
+        document.addEventListener('mousemove', onMouseMove);
+      
+        // drop the element, remove unneeded handlers
+        element.onmouseup = function() {
+          document.removeEventListener('mousemove', onMouseMove);
+          element.onmouseup = null;
+        };
+}
